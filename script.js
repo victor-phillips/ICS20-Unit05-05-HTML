650a0d377b677653a777bc6e36b01744ccce0257
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-18 13:49:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// sets the values of numbers 1 and 2
+let num1 = 0  
+let num2 = 0
+let answer = 0
+// detects when button is pressed and calculates the result
+document.getElementById('button').addEventListener('click', solve)
+// calculates the area
+function solve () {
+  num1 = document.getElementById('length').value
+  num2 = document.getElementById('width').value
+  answer = num1 * num2
+  document.getElementById('answer').innerHTML = answer
+}
